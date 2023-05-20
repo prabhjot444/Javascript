ab9a582a03c529441c01efcfa331834d5cf69a5b
prabhjot444 <117706345+prabhjot444@users.noreply.github.com>
2023-05-20 09:36:38 +0530

    Arrays - beginer

---
+++ array.mjs
@@ -0,0 +1,54 @@
+
+// delete method
+// let d = array
+// delete d[2]
+// console.log(array[])
+
+// concat operator
+// let concat = array.concat()
+let array1 = [1,2,3]
+let array2 = [4,5,6,]
+let array3 = [7,8,9]
+// let allarray = {array1,array2,array3}
+// array1.concat(array2,array3)
+
+// if(allarray.methodName == concat()){
+//     console.log("method name of array is concat()")
+// }
+
+
+console.log("consoling done")
+
+
+let array = ["preet",21,true,99]
+
+// # methods 
+// # to string method
+let tostring = array.toString()
+console.log(tostring)
+
+// join method
+let join = array.join(",")
+console.log(join)
+
+// pop method
+let pop = array.pop()
+console.log(pop)
+
+// push method
+let push = array.push(98)
+console.log(push)
+
+// shift method
+shift  =  array.shift()
+console.log(shift)
+// outputing element 2 instead of element 1
+if(shift.index == 0){
+    console.log(shift[1])
+}
+
+// unshift method
+unshift = array.unshift()
+console.log(unshift)
+
+// concat method
